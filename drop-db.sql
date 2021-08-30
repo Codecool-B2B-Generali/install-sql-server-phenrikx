6947928869ed8d7de833cc58080ec93d4bf03861
Pongrácz Henrik (Y088178) <henrik.pongracz@generali.com>
2021-08-30 11:42:30 +0200

    drop-db

---
+++ drop-db.sql
@@ -0,0 +1,11 @@
+USE [master]
+GO
+
+alter database [BikeStores] set single_user with rollback immediate
+GO
+
+/****** Object:  Database [BikeStores]    Script Date: 2021.08.30 11:18:02 ******/
+DROP DATABASE [BikeStores]
+GO
+
+
